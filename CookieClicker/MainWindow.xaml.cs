b9aa3b4ea3f49a3344f62a98d1718b423d89111d
Murat Varen <bebbobya@gmail.com>
2023-12-01 17:57:45 +0100

    COOKIE CLICKER-04: Klik effect door ThicknessAnimation. (Geen bugs dus geen nood aan ID-05)

--- CookieClicker/MainWindow.xaml.cs
+++ CookieClicker/MainWindow.xaml.cs
@@ -9,6 +9,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -27,7 +28,28 @@ namespace CookieClicker
 
         private void ImgCookie_MouseDown(object sender, MouseButtonEventArgs e)
         {
+            ClickAnimation();
+        }
+        public void ClickAnimation()
+        {
+            // Cookie animatie = margin manipulatie en cookie image verandert
+            BitmapImage bitmapImage = new BitmapImage(new Uri("Assets/Images/Cookie_Cute.png", UriKind.RelativeOrAbsolute));
+            ImgCookie.Source = bitmapImage;
+            ThicknessAnimation clickAnimation = new ThicknessAnimation()
+            {
+                To = new Thickness(15),
+                Duration = TimeSpan.FromMilliseconds(110),
+                FillBehavior = FillBehavior.Stop,
+            };
+            clickAnimation.Completed += ClickAnimation_Completed;
 
+            ImgCookie.BeginAnimation(Image.MarginProperty, clickAnimation);
+        }
+        private void ClickAnimation_Completed(object sender, EventArgs e)
+        {
+            // Animatie complete => Originele cookie image
+            BitmapImage bitmapImage = new BitmapImage(new Uri("Assets/Images/Cookie.png", UriKind.RelativeOrAbsolute));
+            ImgCookie.Source = bitmapImage;
         }
     }
 }
